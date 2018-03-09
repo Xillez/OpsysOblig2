7f4b62b9e9d9349851a97a540f5e9a44a639e400
kentwh <kentwh@stud.ntnu.no>
2018-03-09 19:46:10 +0100

    Modified 'Task2/chromemajPF.bash' to accommodate for shellcheck warnings

--- Task2/chromemajPF.bash
+++ Task2/chromemajPF.bash
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-PID_LIST=$(pgrep "chrome")
-
-echo "$(pgrep code)" | while read item; do
-    PF_PID=$(ps --no-headers -o maj_flt $item)
+pgrep code | while read -r item; do
+    PF_PID=$(ps --no-headers -o maj_flt "$item")
     
     if [[ $PF_PID -gt 1000 ]]; then
         echo -n "Pid $item generated $PF_PID page faults (over 1000)!!
