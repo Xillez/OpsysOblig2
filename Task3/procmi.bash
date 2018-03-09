6981793c02ba5a6fe8903265e20eafe89a708bef
kentwh <kentwh@stud.ntnu.no>
2018-03-09 19:50:40 +0100

    Modified 'Task3/procmi.bash' to accomodate for shellcheck warnings

--- Task3/procmi.bash
+++ Task3/procmi.bash
@@ -11,10 +11,10 @@ do
 
     # Loop through all items in "ITEM_NAMES" and find them in "/proc/$PID/status"
     i=0
-    for item in ${ITEM_NAMES[@]}
+    for item in "${ITEM_NAMES[@]}"
     do
         # Find the item value (from "/proc/$PID/status") and save it (in "ITEM_VALUES")
-        ITEM_VALUES[$item]=$(cat "/proc/$PID/status" | grep ${ITEM_NAMES[i]} | awk '{ print $2 }')
+        ITEM_VALUES[$item]=$(grep "${ITEM_NAMES[i]}" "/proc/$PID/status" | awk '{ print $2 }')
         ((i++))
     done
 
@@ -24,5 +24,5 @@ Total bruk av virtuelt minne (VmSize):   ${ITEM_VALUES[${ITEM_NAMES:0}]} KB
 Mengde privat virtuelt minne (VmData+VmStk+VmExe):  $(echo "${ITEM_VALUES[${ITEM_NAMES[1]}]} + ${ITEM_VALUES[${ITEM_NAMES[2]}]} + ${ITEM_VALUES[${ITEM_NAMES[3]}]}" | bc) KB
 Mengde shared virtuelt minne (VmLib):   ${ITEM_VALUES[${ITEM_NAMES[4]}]} KB
 Total bruk av fysisk minne (VmRSS):   ${ITEM_VALUES[${ITEM_NAMES[5]}]} KB
-Mengde fysisk minne som benyttes til page table (VmPTE):   ${ITEM_VALUES[${ITEM_NAMES[6]}]} KB" > $FILE_NAME
+Mengde fysisk minne som benyttes til page table (VmPTE):   ${ITEM_VALUES[${ITEM_NAMES[6]}]} KB" > "$FILE_NAME"
 done
